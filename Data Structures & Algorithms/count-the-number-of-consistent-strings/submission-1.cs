bf2a6fabcbbefd6c4c6c84bdec2446415b2a4f74
Brandon Moreno <123613562+Entitycrethus@users.noreply.github.com>
2026-08-24 08:06:34 -0500

    Add: count-the-number-of-consistent-strings - submission-1

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int CountConsistentStrings(string allowed, string[] words) {
+
+        int[] code = new int[26];
+
+        int count = 0;
+
+        foreach(char c in allowed){
+            code[c - 'a']=1;
+        }   
+
+        foreach(string s in words){
+            bool consistent = true;
+
+                foreach(char c in s){
+                    if( code[c-'a'] == 0){
+                        consistent = false;
+                        break;
+                    }
+                }    
+            if(consistent){
+                count++;
+            }
+
+        }
+        
+        return count;
+
+
+    }
+}
